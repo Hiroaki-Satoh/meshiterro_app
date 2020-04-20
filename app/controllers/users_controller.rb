a7299978d01b903c17b01597bc3cc481fb31f800
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-04-20 19:05:12 +0900

    [Finish] 5-6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   	# 投稿画像の表示制限
-	@post_images = @user.post_images.page(params[:page]).reverse_order  # ここを記述
+	  @post_images = @user.post_image.page(params[:page]).reverse_order  # ここを記述
   end
 
   def edit
